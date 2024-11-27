7bea2a73e6b12e82f9ff867b95c625acc43dd597
bajajku <bajajku@sheridancollege.ca>
2024-11-27 18:19:15 -0500

    Modified ViewModel to support forecast instead of current

--- HourlyWTrackerKunal/ViewModels/WeatherViewModel.swift
+++ HourlyWTrackerKunal/ViewModels/WeatherViewModel.swift
@@ -5,18 +5,15 @@ import Foundation
 
 class WeatherViewModel: ObservableObject, Identifiable {
     
-    @Published var weather: Weather?
+    @Published var weather: WeatherForecast?
     
     
-    
-    let baseURL: String = "https://api.weatherapi.com/v1/current.json?"
+    let baseURL: String = "https://api.weatherapi.com/v1/forecast.json?"
     let key: String = "0f9872779b184f46b1c185742241011"
     
-    func getWeather(for coordinates: (Double, Double) , aqi: String, completion: @escaping (Result<Data, Error>) -> Void) {
+    func getWeather(for city: String , aqi: String, completion: @escaping (Result<Data, Error>) -> Void) {
         
-        let lat = coordinates.0
-        let lon = coordinates.1
-        let urlString = baseURL + "key=\(key)&q=\(lat),\(lon)&aqi=\(aqi)"
+        let urlString = baseURL + "key=\(key)&q=\(city)&aqi=\(aqi)&alerts=no"
         print("URL: \(urlString)")
         guard let url = URL(string: urlString) else {
             print("Invalid URL")
@@ -32,7 +29,7 @@ class WeatherViewModel: ObservableObject, Identifiable {
             else if let data{
                 print("Data: \(String(data: data, encoding: .utf8)!)")
                 do{
-                    let decodedWeather = try JSONDecoder().decode(Weather.self, from: data)
+                    let decodedWeather = try JSONDecoder().decode(WeatherForecast.self, from: data)
                     DispatchQueue.main.async {
                         self.weather = decodedWeather
                     }
