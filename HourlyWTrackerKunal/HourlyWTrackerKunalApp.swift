80a011192563d28a4d0b49b51c7faa6a8c60fb89
bajajku <bajajku@sheridancollege.ca>
2024-11-27 14:27:43 -0500

    Initial Commit

---
+++ HourlyWTrackerKunal/HourlyWTrackerKunalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyWTrackerKunalApp.swift
+//  HourlyWTrackerKunal
+//
+//  Created by Kunal Bajaj on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct HourlyWTrackerKunalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HourlyWTrackerKunalTests/HourlyWTrackerKunalTests.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyWTrackerKunalTests.swift
+//  HourlyWTrackerKunalTests
+//
+//  Created by Kunal Bajaj on 2024-11-27.
+//
+
+import Testing
+@testable import HourlyWTrackerKunal
+
+struct HourlyWTrackerKunalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
