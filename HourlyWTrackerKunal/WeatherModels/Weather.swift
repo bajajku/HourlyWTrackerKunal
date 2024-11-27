c335c3e2ebf640546afb674004247b8201639fc0
bajajku <bajajku@sheridancollege.ca>
2024-11-27 17:55:13 -0500

    Added models from last assignment

---
+++ HourlyWTrackerKunal/WeatherModels/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherAPI.swift
+//  KunalWeather
+//
+//  Created by Kunal Bajaj on 2024-11-10.
+//  991648986
+
+import Foundation
+
+class Weather: Codable{
+    let location: Location
+    let current: Current
+}
+
